739bdf4b33a5dbb6f02144937c7d43b73e5ae374
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2025-08-06 22:07:06 +0200

    Add styles to div.tasks in ProjectSeleceted

--- src/components/ProjectSelected.jsx
+++ src/components/ProjectSelected.jsx
@@ -21,11 +21,11 @@ export default function ProjectSelected({
     <main className="flex-[4] flex flex-col gap-4 lg:gap-16 justify-beetwen text-xs lg:text-lg p-4 lg:pt-10">
       <div className="info  border-b-2 border-black">
         <header className="flex justify-between">
-          <h2 className="text-base lg:text-6xl font-bold capitalize">
+          <h2 className="text-base lg:text-6xl font-bold uppercase">
             {activeProject.name}
           </h2>
           <button
-            className="text-[0.65rem] lg:text-base"
+            className="text-xs lg:text-lg mr-8"
             onClick={() => deleteProject(activeProject.id)}
           >
             Delete
@@ -37,13 +37,13 @@ export default function ProjectSelected({
         <h3 className="mt-2 lg:mt-4 text-sm lg:text-3xl font-semibold">
           About project
         </h3>
-        <p className="lg:text-lg">{activeProject.description}</p>
+        <p className="lg:text-lg pb-4 lg:pb-8">{activeProject.description}</p>
       </div>
-      <div className="tasks text-center">
-        <h2>Tasks</h2>
+      <div className="tasks ml-2 lg:ml-8">
+        <h2 className="font-bold text-sm lg:text-3xl mb-2 lg:mb-4">Tasks</h2>
         <input
           type="text"
-          className="border-[#333] border rounded-sm p-1"
+          className="bg-neutral-300 focus:bg-transparent transition rounded-sm p-1 mr-2 lg:mr-4"
           ref={input}
         />
         <button onClick={handleAddTaskClick}>Add task</button>
@@ -54,23 +54,26 @@ export default function ProjectSelected({
         >
           Input cannot be empty
         </p>
-        <h3>Task list</h3>
-        <ol>
-          {activeProject.tasks.map((task) => {
-            return (
-              <li key={task.id}>
-                <span>{task.taskText}</span>
-                <button
-                  onClick={() => {
-                    handleDeleteTask(activeProject.id, task.id);
-                  }}
-                >
-                  Delete
-                </button>
-              </li>
-            );
-          })}
-        </ol>
+        <h3 className="font-semibold lg:text-2xl mb-2">Task list</h3>
+        {activeProject.tasks.length > 0 && (
+          <ol className="bg-neutral-300 p-2 lg:p-4">
+            {activeProject.tasks.map((task) => {
+              return (
+                <li className="flex justify-between" key={task.id}>
+                  <span>{task.taskText}</span>
+                  <button
+                    className="hover:text-red-600 transition"
+                    onClick={() => {
+                      handleDeleteTask(activeProject.id, task.id);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </li>
+              );
+            })}
+          </ol>
+        )}
       </div>
     </main>
   );
